de6bdb25b8e78bc54f7330e6cd5ca8fc2c915cb9
Veronica Busiello <vbusiello@outlook.com>
2017-09-19 19:04:41 -0300

    Agregue mis cambios

--- InterfazWeb/AddProv.aspx
+++ InterfazWeb/AddProv.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="AddProv.aspx.cs" Inherits="InterfazWeb.AddProv" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddProv.aspx.cs" Inherits="InterfazWeb.WebForm1" %>
 
 <!DOCTYPE html>
 
@@ -9,6 +9,35 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:Label ID="Label6" runat="server" Text="Registro de proveedores"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Rut"></asp:Label>
+&nbsp;<asp:TextBox ID="txtRut" runat="server" Width="118px"></asp:TextBox>
+            <asp:CustomValidator ID="valRut" runat="server" ControlToValidate="txtRut" ErrorMessage="Ingrese un rut valido" OnServerValidate="ValRut_ServerValidate" SetFocusOnError="True" ValidateEmptyText="True"></asp:CustomValidator>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Clave"></asp:Label>
+&nbsp;<asp:TextBox ID="txtClave" runat="server" TextMode="Password"></asp:TextBox>
+&nbsp;<asp:CustomValidator ID="valClave" runat="server" ControlToValidate="txtClave" ErrorMessage="La clave debe contener entre 8 y 15 caracteres, letras mayusculas y minusculas" OnServerValidate="valClave_ServerValidate" ValidateEmptyText="True"></asp:CustomValidator>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Nombre fantasia"></asp:Label>
+&nbsp;<asp:TextBox ID="txtNombre" runat="server" Width="118px"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="valNombre" runat="server" ErrorMessage="Ingrese un nombre" ControlToValidate="txtNombre"></asp:RequiredFieldValidator>
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Email"></asp:Label>
+&nbsp;<asp:TextBox ID="txtEmail" runat="server" Width="118px"></asp:TextBox>
+            <asp:CustomValidator ID="valEmail" runat="server" ErrorMessage="Ingrese un mail valido" ControlToValidate="txtEmail" OnServerValidate="valEmail_ServerValidate" ValidateEmptyText="True"></asp:CustomValidator>
+            <br />
+            <asp:Label ID="Label5" runat="server" Text="Telefono"></asp:Label>
+&nbsp;<asp:TextBox ID="txtTel" runat="server" Width="118px"></asp:TextBox>
+            <asp:CustomValidator ID="valTel" runat="server" ErrorMessage="Ingrese un telefono valido" ControlToValidate="txtTel" OnServerValidate="valTel_ServerValidate" ValidateEmptyText="True"></asp:CustomValidator>
+            <br />
+            <asp:CheckBox ID="chkVip" runat="server" Text="Vip" />
+            <br />
+            <br />
+            <asp:Button ID="btnRegistro" runat="server" Text="Registrar" OnClick="btnRegistro_Click" />
+            <br />
+            <asp:Label ID="lblResultado" runat="server"></asp:Label>
         </div>
     </form>
 </body>
--- InterfazWeb/AddProv.aspx.designer.cs
+++ InterfazWeb/AddProv.aspx.designer.cs
@@ -10,7 +10,7 @@
 namespace InterfazWeb {
     
     
-    public partial class AddProv {
+    public partial class WebForm1 {
         
         /// <summary>
         /// form1 control.
@@ -20,5 +20,176 @@ namespace InterfazWeb {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Label6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// txtRut control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRut;
+        
+        /// <summary>
+        /// valRut control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator valRut;
+        
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// txtClave control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtClave;
+        
+        /// <summary>
+        /// valClave control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator valClave;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// txtNombre control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+        
+        /// <summary>
+        /// valNombre control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator valNombre;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// txtEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        
+        /// <summary>
+        /// valEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator valEmail;
+        
+        /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// txtTel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTel;
+        
+        /// <summary>
+        /// valTel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator valTel;
+        
+        /// <summary>
+        /// chkVip control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkVip;
+        
+        /// <summary>
+        /// btnRegistro control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegistro;
+        
+        /// <summary>
+        /// lblResultado control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblResultado;
     }
 }
