cde6dbe98cf7718c5dc6147b1cf625f434d869cc
gotheguy <31864777+gotheguy@users.noreply.github.com>
2017-09-22 00:30:12 -0300

    Add files via upload

---
+++ WCFProveedorXRut/IServicioProveedorXRut.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using EntidadesNegocio;
+
+namespace WCFProveedorXRut
+{
+    [ServiceContract]
+    public interface IServicioProveedorXRut
+    {
+        [OperationContract]
+        List<Proveedor> FindById(string rut);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WCFProveedorXRut/ServicioProveedorXRut.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using EntidadesNegocio;
+
+namespace WCFProveedorXRut
+{
+    //public class ServicioProveedorXRut : IServicioProveedorXRut
+    //{
+    //    public List<Proveedor> FindAll()
+    //    {
+    //        //List<Proveedor> lista = Proveedor.FindById(rut);
+    //        //return lista;
+    //    }
+    //}
+}
