fe9b1801c840dccabf3da7b9d2afc7ae26e53e58
gotheguy <31864777+gotheguy@users.noreply.github.com>
2017-09-24 03:09:41 -0300

    Add files via upload

---
+++ WCFDesactivarProveedor/IServicioDesactivarProveedor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using EntidadesNegocio;
+
+namespace WCFDesactivarProveedor
+{
+    [ServiceContract]
+    public interface IServicioDesactivarProveedor
+    {
+        [OperationContract]
+        bool DesactivarProveedor(string rut, bool activo);
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        string rut = string.Empty;
+        bool activo = false;
+
+        [DataMember]
+        public string Rut
+        {
+            get { return rut; }
+            set { rut = value; }
+        }
+
+        [DataMember]
+        public bool Activo
+        {
+            get { return activo; }
+            set { activo = value; }
+        }
+    }
+}
---
+++ WCFDesactivarProveedor/ServicioDesactivarProveedor.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using EntidadesNegocio;
+
+namespace WCFDesactivarProveedor
+{
+    public class ServicioDesactivarProveedor : IServicioDesactivarProveedor
+    {
+        public bool DesactivarProveedor(string rut, bool activo)
+        {
+            bool result = false;
+
+            result = Proveedor.Desactivar(rut, activo);
+
+            return result;
+        }
+    }
+}
