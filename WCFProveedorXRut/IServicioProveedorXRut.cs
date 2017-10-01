3ac322889a7deb7b9afdd90afa1fd7e9e7802003
gotheguy <31864777+gotheguy@users.noreply.github.com>
2017-10-01 00:16:01 -0300

    Add files via upload

---
+++ WCFProveedorXRut/IServicioProveedorXRut.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using EntidadesNegocio;
+
+namespace WCFProveedorXRut
+{
+    [ServiceContract]
+    public interface IServicioProveedorXRut
+    {
+        [OperationContract]
+        Proveedor_Vip FindById(string rut);
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        string rut = string.Empty;
+        string nombrefantasia = string.Empty;
+        string email = string.Empty;
+        string telefono = string.Empty;
+
+        [DataMember]
+        public string Rut
+        {
+            get { return rut; }
+            set { rut = value; }
+        }
+
+        [DataMember]
+        public string NombreFantasia
+        {
+            get { return nombrefantasia; }
+            set { nombrefantasia = value; }
+        }
+
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        [DataMember]
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+    }
+}
---
+++ WCFProveedorXRut/ServicioProveedorXRut.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using EntidadesNegocio;
+
+namespace WCFProveedorXRut
+{
+    public class ServicioProveedorXRut : IServicioProveedorXRut
+    {
+        public Proveedor_Vip FindById(string rut)
+        {
+            Proveedor_Vip pv = Proveedor_Vip.FindById(rut);
+            return pv;
+        }
+    }
+}
