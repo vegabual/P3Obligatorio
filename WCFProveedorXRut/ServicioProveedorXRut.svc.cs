00c5b931ec2f3d1e05aa0731a08134f8feb45780
Oth1 <gonzalo.otheguy@hotmail.com>
2017-10-02 23:07:45 -0300

    quedo hecho provxrut

--- WCFProveedorXRut/ServicioProveedorXRut.svc.cs
+++ WCFProveedorXRut/ServicioProveedorXRut.svc.cs
@@ -1,4 +1,10 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
 using EntidadesNegocio;
 
 namespace WCFProveedorXRut
